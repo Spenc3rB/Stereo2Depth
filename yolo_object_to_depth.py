f18f99b8c75bc6b365550cf5ed0e6b54d96b59e0
Spenc3rB <beersc@colostate.edu>
2023-12-06 12:25:35 -0700

    normalizing the frames in yolo---.py script

--- yolo_object_to_depth.py
+++ yolo_object_to_depth.py
@@ -46,6 +46,7 @@ while(cap_right.isOpened() and cap_left.isOpened()):
 
     # Convert the frame_right to float32 and normalize the values
     frame_right = np.float32(frame_right)/255.0
+    frame_right = np.expand_dims(frame_right, axis=0)
 
     # If cannot catch any frame, break
     if succes_left and succes_right:                    
